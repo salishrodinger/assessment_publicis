5354793d1cbcf9e5dbd95224038979d8ece517ac
Sarah <86535632+salishrodinger@users.noreply.github.com>
2023-01-19 17:17:36 +0100

    Update sunburst_chart.R

--- sunburst_chart.R
+++ sunburst_chart.R
@@ -1,10 +1,10 @@
 install.packages("sunburstR")
 library(sunburstR)
-#read in the .csv file to a data frame
+#lecture du fichier csv
 path_data <- read.csv(
-  "/Users/sarahlinalachheb/Downloads/test_technique/bquxjob_3d9765be_185c749b3cc.csv"
+  "bquxjob_3d9765be_185c749b3cc.csv"
   ,header=T
   ,stringsAsFactors = FALSE
 )
-#create sunburst visualisation
+# création du sunburst
 sunburst(path_data,percent=TRUE,count=TRUE)
