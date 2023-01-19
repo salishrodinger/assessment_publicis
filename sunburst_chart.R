6b49a8ed8354d6eb6b04fb49ef7bc95b056ce49f
Sarah <86535632+salishrodinger@users.noreply.github.com>
2023-01-19 01:05:13 +0100

    Update sunburst_chart.R

--- sunburst_chart.R
+++ sunburst_chart.R
@@ -2,13 +2,9 @@ install.packages("sunburstR")
 library(sunburstR)
 #read in the .csv file to a data frame
 path_data <- read.csv(
-  "/Users/sarahlinalachheb/Downloads/test_technique/bquxjob_7089cc7e_185c5e08138.csv"
+  "/Users/sarahlinalachheb/Downloads/test_technique/bquxjob_3d9765be_185c749b3cc.csv"
   ,header=T
   ,stringsAsFactors = FALSE
 )
-
-#select the 2 columns needed for the visualisation
-df <- path_data[c('full_journey','nb_parcours')]
-
 #create sunburst visualisation
-sunburst(df,percent=TRUE,count=TRUE)
+sunburst(path_data,percent=TRUE,count=TRUE)
