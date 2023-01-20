07c44c806142c46ccdd50a04decd9669a41a9417
Sarah <86535632+salishrodinger@users.noreply.github.com>
2023-01-20 19:13:00 +0100

    Add legend cleaning

--- sunburst_chart.R
+++ sunburst_chart.R
@@ -1,10 +1,14 @@
 install.packages("sunburstR")
 library(sunburstR)
-#lecture du fichier csv
+# Lecture du fichier csv
 path_data <- read.csv(
-  "bquxjob_3d9765be_185c749b3cc.csv"
-  ,header=T
-  ,stringsAsFactors = FALSE
-)
-# création du sunburst
-sunburst(path_data,percent=TRUE,count=TRUE)
+  "bquxjob_3d9765be_185c749b3cc.csv",
+  header = TRUE,
+  stringsAsFactors = FALSE)
+
+# Cleaning des libellés des légendes
+path_data$full_journey <- gsub("_", " ", path_data$full_journey)
+path_data$full_journey <- toupper(path_data$full_journey)
+
+# Création du sunburst
+sunburst(path_data, percent = TRUE, count = TRUE)
